50493c0d1fd3fa6b95cec94fbc45d339231414d6
Brian Hines <brian@projectweekend.net>
2014-02-25 11:00:47 -0600

    Start organizing event flow for initializing HUE connection

--- main.py
+++ main.py
@@ -3,22 +3,30 @@ import time
 import sensors
 import events
 
-
+HUE_SYSTEM_IS_READY = False
 DETECTION_TIMEOUT = 60
 LUMINOSITY_THRESHOLD = 10
 
 
 if __name__ == "__main__":
 
+    # main loop
     while True:
 
-        if sensors.detect_motion():
+        if not HUE_SYSTEM_IS_READY:
+            # Run initialization process here
+            # This will have it's own loop and will not exit 
+            # until completed successfully. Set HUE_SYSTEM_IS_READY = True
+            pass
+        
+        if HUE_SYSTEM_IS_READY:
+            
+            if sensors.detect_motion():
+                print("MOTION DETECTED!")
+                events.log_motion_event()
 
-            print("MOTION DETECTED!")
-            events.log_motion_event()
+                if sensors.read_luminosity() < LUMINOSITY_THRESHOLD:
+                    # TODO: Make call to HUE API to turn on lights
+                    print("LIGHTS ON!")
 
-            if sensors.read_luminosity() < LUMINOSITY_THRESHOLD:
-                # TODO: Make call to HUE API to turn on lights
-                print("LIGHTS ON!")
-
-            time.sleep(DETECTION_TIMEOUT)
+                time.sleep(DETECTION_TIMEOUT)
