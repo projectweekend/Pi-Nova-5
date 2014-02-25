9cc1bca1f6beeab15fee3bb958eb679a121f12ca
Brian Hines <brian@projectweekend.net>
2014-02-24 21:39:31 -0600

    Add luminosity check to main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import events
 
 
 DETECTION_TIMEOUT = 60
+LUMINOSITY_THRESHOLD = 10
 
 
 if __name__ == "__main__":
@@ -12,6 +13,13 @@ if __name__ == "__main__":
     while True:
 
         if sensors.detect_motion():
+
             print("MOTION DETECTED!")
             events.log_motion_event()
+
+            current_luminosity = sensors.read_luminosity()
+            if current_luminosity < LUMINOSITY_THRESHOLD:
+                # TODO: Make call to HUE API to turn on lights
+                pass
+            
             time.sleep(DETECTION_TIMEOUT)
