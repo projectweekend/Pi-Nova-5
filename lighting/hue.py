f54a405fb3a31149b804e101a4af86fdee833088
Brian Hines <brian@projectweekend.net>
2014-02-27 20:51:09 -0600

    Refactor hue API calls to use Hammock

--- lighting/hue.py
+++ lighting/hue.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from hammock import Hammock
 from custom_exceptions import *
 
 
@@ -12,32 +13,29 @@ HUE_AUTHORIZED_USER_CONFIG = {
 
 class Light(object):
     
-    def __init__(self, bridge_ip, username, light_id):
+    def __init__(self, api, username, light_id):
         self.id = light_id
-        self.url = "http://{0}/api/{1}/lights/{2}/state".format(bridge_ip, username, light_id)
+        self.username = username
+        self.api = api
 
     def turn_on(self):
-        put_headers = {'content-type': 'application/json'}
-        put_body = json.dumps({'on': True})
-        requests.put(url=self.url, data=put_body, headers=put_headers)
+        self.api(self.username).lights(self.id).state.PUT(data=json.dumps({'on': True}))
 
 
 class Bridge(object):
 
-    ip_address = ""
-    username = ""
-    authorized = False
-    press_link = False
-    lights = {}
-
-    def __init__(self, ip=""):
+    def __init__(self, ip="", user_config=HUE_AUTHORIZED_USER_CONFIG):
 
         if ip:
             self.ip_address = ip
         else:
             self._find_ip_address()
 
-        self.username = HUE_AUTHORIZED_USER_CONFIG['username']
+        self.api = Hammock("http://{0}/api".format(self.ip_address))
+        self.user_config = user_config
+        self.authorized = False
+        self.press_link = False
+        self.lights = {}
         self._check_authorization()
 
     # Set the ip_address using HUE API web utility
@@ -48,36 +46,31 @@ class Bridge(object):
             message = "IP utility was not available at: {0}".format(IP_UTILITY_URL)
             raise IPUtilityException(message)
 
-        response_data = utility_response.json()
-        if not response_data:
+        data = utility_response.json()
+        if not data:
             message = "No bridge was located on this network"
             raise BridgeNotFoundException(message)
 
-        self.ip_address = response_data[0]['internalipaddress']
+        self.ip_address = data[0]['internalipaddress']
 
     def _check_authorization(self):
 
-        if not self.ip_address:
-            message = "The 'ip_address' property is empty"
-            raise BridgeConfigurationException(message)
+        r = self.api(self.user_config['username']).GET()
 
-        authorization_check_url = "http://{0}/api/{1}".format(self.ip_address, self.username)
-        authorization_check_response = requests.get(authorization_check_url)
-
-        if authorization_check_response.status_code != 200:
-            message = "The bridge did not respond properly on route '{0}'".format(authorization_check_url)
+        if r.status_code != 200:
+            message = "The bridge did not respond properly on route '{0}'".format(r.url)
             raise BridgeAPIResponseException(message)
 
-        response_data = authorization_check_response.json()
+        data = r.json()
 
         # if authorized, the response is a dictionary of light data
-        if isinstance(response_data, dict):
+        if isinstance(data, dict):
             self.authorized = True
             self._find_lights()
             return
 
         # if unauthorized, then response is a list with an error dictionary inside
-        unauthorized_error = response_data[0].get('error', '')
+        unauthorized_error = data[0].get('error', '')
         if unauthorized_error and unauthorized_error['type'] == 1:
             self.authorized = False
         else:
@@ -87,33 +80,29 @@ class Bridge(object):
             raise BridgeAPIResponseException(message)
 
     def _find_lights(self):
-        
-        lights_url = "http://{0}/api/{1}/lights".format(self.ip_address, self.username)
-        lights_response = requests.get(lights_url)
 
-        if lights_response.status_code != 200:
-            message = "The bridge did not respond properly on route '{0}'".format(lights_url)
+        r = self.api(self.user_config['username']).lights.GET()
+
+        if r.status_code != 200:
+            message = "The bridge did not respond properly on route '{0}'".format(r.url)
             raise BridgeAPIResponseException(message)
 
-        response_data = lights_response.json()
-        for k in response_data.keys():
-            self.lights[k] = Light(self.ip_address, self.username, k)
+        data = r.json()
+        for k in data.keys():
+            self.lights[k] = Light(self.api, self.user_config['username'], k)
 
     def authorize(self):
 
         if not self.authorized:
-            post_headers = {'content-type': 'application/json'}
-            post_body = json.dumps(HUE_AUTHORIZED_USER_CONFIG)
-            post_url = "http://{0}/api".format(self.ip_address)
 
-            authorization_response = requests.post(url=post_url, data=post_body, headers=post_headers)
+            r = self.api.POST(data=json.dumps(HUE_AUTHORIZED_USER_CONFIG))
 
-            if authorization_response.status_code != 200:
-                message = "The bridge did not respond properly on route: '{0}'".format(post_url)
+            if r.status_code != 200:
+                message = "The bridge did not respond properly on route: '{0}'".format(r.url)
                 raise BridgeAPIResponseException(message)
 
-            response_data = authorization_response.json()
-            unauthorized_error = response_data[0].get('error', '')
+            data = r.json()
+            unauthorized_error = data[0].get('error', '')
 
             if not unauthorized_error:
                 self.authorized = True
