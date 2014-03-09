58372abbb9431f488b35f4c8c879906813bab17e
Brian Hines <brian@projectweekend.net>
2014-03-09 10:38:50 -0500

    Log datetime consistently with other Pi's in the network

--- events/motion.py
+++ events/motion.py
@@ -4,4 +4,4 @@ from stash import Stash
 
 def log_motion_event():
     motion_events = Stash('motion_events')
-    motion_events.add({'date': datetime.now()})
+    motion_events.add({'date': datetime.utcnow().isoformat()})
