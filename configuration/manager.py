f68ec4966c0844a7b2553dc59975b21268720d33
Brian Hines <brian@projectweekend.net>
2014-03-12 18:27:21 -0500

    Add some more stuff to config manager

--- configuration/manager.py
+++ configuration/manager.py
@@ -14,17 +14,28 @@ class ConfigurationManager(object):
     def __init__(self, api_url, system_name):
         self._api = Hammock(api_url)
         self._system_name = system_name
-        self._get_stash()
+        self._stash = self._get_stash()
+        self.load_data()
 
-    def load(self):
+    def read(self, key):
+        return self.config_data.get(key, None)
+
+    def load_data(self):
         if self._stash.data:            
-            self.config_data = self._stash.data.pop()
-            self._stash.empty()
+            self.config_data = self._stash.data[-1]        
 
-    def save(self):
-        self._stash.add(self.config_data)
+    def get_updates(self):
+        if self._pull():
+            self._save()
+            self.load_data()
+            return True
+        return False
 
-    def pull(self):
+    def _save(self):
+        self._stash.empty()
+        self._stash.add(self.config_data)        
+
+    def _pull(self):
         url_params = {'system_name': self._system_name}
         response = self.api.GET(params=url_params)
         if response.status_code != 200:
@@ -32,17 +43,11 @@ class ConfigurationManager(object):
             self._send_failure_message(message_body.format())
             return False
         self._load_config_data(response.json())
-        return True
-
-    def rebuild(self):
-        if self.pull():
-            self.save()
-            return True
-        return False
+        return True        
 
     def _get_stash(self):
         stash_file_name = "{0}_configuration_data".format(self._system_name.lower())
-        self._stash = Stash(stash_file_name)
+        return Stash(stash_file_name)
 
     def _load_config_data(self, json_data):
         if json_data:
