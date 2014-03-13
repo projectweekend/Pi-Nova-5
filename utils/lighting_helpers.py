96a2f361f9bab2842b247a6916b2164c6da7f793
Brian Hines <brian@projectweekend.net>
2014-03-13 07:35:21 -0500

    Rename util function

--- utils/lighting_helpers.py
+++ utils/lighting_helpers.py
@@ -5,7 +5,7 @@ API_URL = 'http://holly.local/api/system/configuration'
 SYSTEM_NAME = 'Nova5'
 
 
-def get_lights_to_turn_on():
+def get_enabled_lights():
     manager = ConfigurationManager(API_URL, SYSTEM_NAME)
     list_of_lights = manager.read('lights_enabled')
     if not list_of_lights:
