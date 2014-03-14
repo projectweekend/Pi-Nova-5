d1f381bed7fc5afe242708063e2f51497ec858a8
Brian Hines <brian@projectweekend.net>
2014-03-14 07:50:20 -0500

    Add a util to check whether or not lights should be turned on based on current time

--- utils/lighting_helpers.py
+++ utils/lighting_helpers.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from configuration import ConfigurationManager
 
 
@@ -11,3 +12,24 @@ def get_enabled_lights():
     if not list_of_lights:
         return []
     return list_of_lights
+
+
+def is_auto_lighting_enabled():
+    manager = ConfigurationManager(API_URL, SYSTEM_NAME)
+    disabled_start = manager.read('disabled_time_start')
+    disabled_end = manager.read('disabled_time_end')
+
+    now = datetime.now()
+
+    on_start_hour = now.hour == disabled_start['hour']
+    past_start_hour = now.hour > disabled_start['hour']
+    past_start_minute = now.minute >= disabled_start['minute']
+
+    on_end_hour = now.hour == disabled_end['hour']
+    before_end_hour = now.hour < disabled_end['hour']
+    before_end_minute = now.hour <= disabled_end['minute']
+
+    if past_start_hour or (on_start_hour and past_start_minute):
+        if before_end_hour or (on_end_hour and before_end_minute):
+            return True
+    return False
