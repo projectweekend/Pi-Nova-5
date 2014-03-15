c067ed2c7f28cb6c54e15d10d72a6a94957a4cdd
Brian Hines <brian@projectweekend.net>
2014-03-14 19:42:45 -0500

    If it's really dark, this thing bugs out

--- sensors/luminosity.py
+++ sensors/luminosity.py
@@ -3,4 +3,8 @@ from adafruit import TSL2561
 
 def read_luminosity():
     tsl = TSL2561()
-    return tsl.read_lux(gain=1)
+    try:
+        luminosity = tsl.read_lux(gain=1)
+    except ZeroDivisionError:
+        luminosity = 0
+    return luminosity
