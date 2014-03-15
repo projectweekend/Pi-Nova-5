4cd9d445c87100f247fdd320f6b06813bf1b508d
Brian Hines <brian@projectweekend.net>
2014-03-14 23:07:25 -0500

    Add some error handling in case requests lib bugs out randomly

--- refresh_config.py
+++ refresh_config.py
@@ -1,3 +1,5 @@
+import time
+
 from configuration import ConfigurationManager
 
 
@@ -6,5 +8,21 @@ SYSTEM_NAME = 'Nova5'
 
 
 if __name__ == "__main__":
+
+    refresh_attempts = 0
+    refresh_complete = False
+
     manager = ConfigurationManager(API_URL, SYSTEM_NAME)
-    manager.get_updates()
+
+    while refresh_complete == False and refresh_attempts < 6:
+        try:
+            manager.get_updates()
+        except:
+            refresh_attempts += 1
+            time.sleep(5)
+        else:
+            refresh_complete = True
+
+    if refresh_complete == False:
+        # TODO log failure with a message to Holly
+        pass
