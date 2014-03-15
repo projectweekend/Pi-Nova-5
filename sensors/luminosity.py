0d70ab961315f0a3f06b8204d95317d391dee9cb
Brian Hines <brian@projectweekend.net>
2014-03-15 17:25:56 -0500

    Derp

--- sensors/luminosity.py
+++ sensors/luminosity.py
@@ -4,7 +4,7 @@ from adafruit import TSL2561
 def read_luminosity():
     tsl = TSL2561()
     try:
-        luminosity = tsl.read_lux(gain=8)
+        luminosity = tsl.read_lux(gain=16)
     except ZeroDivisionError:
         luminosity = 0
     return luminosity
