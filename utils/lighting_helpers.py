a725011d27c288ada7c6ffc3a36f05f168049ea4
Brian Hines <brian@projectweekend.net>
2014-03-14 19:54:15 -0500

    Don't expose the manager property on this class

--- utils/lighting_helpers.py
+++ utils/lighting_helpers.py
@@ -11,27 +11,27 @@ class LightingConfig(object):
 
     def __init__(self):
         
-        self.manager = ConfigurationManager(API_URL, SYSTEM_NAME)
+        self._manager = ConfigurationManager(API_URL, SYSTEM_NAME)
 
     def enabled_lights(self):
-        list_of_lights = self.manager.read('lights_enabled')
+        list_of_lights = self._manager.read('lights_enabled')
         if not list_of_lights:
             return []
         return list_of_lights
 
     def luminosity_threshold(self):
-        luminosity_threshold = self.manager.read('luminosity_threshold')
+        luminosity_threshold = self._manager.read('luminosity_threshold')
         if not luminosity_threshold:
             return LUMINOSITY_THRESHOLD_DEFAULT
         return luminosity_threshold
 
     def auto_lighting_disabled(self):
 
-        if self.manager.read('manually_disabled'):
+        if self._manager.read('manually_disabled'):
             return True
 
-        disabled_start = self.manager.read('disabled_time_start')
-        disabled_end = self.manager.read('disabled_time_end')
+        disabled_start = self._manager.read('disabled_time_start')
+        disabled_end = self._manager.read('disabled_time_end')
 
         now = datetime.now()
 
