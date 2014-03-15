836d0f5d25939cc4089b20c4d3763c21b9f4fb72
Brian Hines <brian@projectweekend.net>
2014-03-15 17:49:06 -0500

    Debugging

--- sensors/luminosity.py
+++ sensors/luminosity.py
@@ -3,8 +3,9 @@ from adafruit import TSL2561
 
 def read_luminosity():
     tsl = TSL2561()
-    try:
-        luminosity = tsl.read_lux(gain=16)
-    except ZeroDivisionError:
-        luminosity = 0
-    return luminosity
+    return tsl.read_lux(gain=16)
+    # try:
+    #     luminosity = tsl.read_lux(gain=16)
+    # except ZeroDivisionError:
+    #     luminosity = 0
+    # return luminosity
