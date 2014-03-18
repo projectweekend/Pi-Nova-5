e2ba4bd66c4ec99e10c29c24b70efc84b068d45f
Brian Hines <brian@projectweekend.net>
2014-03-18 09:56:30 -0500

    Move to util

--- main.py
+++ main.py
@@ -3,63 +3,22 @@ import time
 import RPi.GPIO as gpio
 
 import events
-import lighting
 import utils
 import adafruit
 from led import LED
 
 DETECTION_TIMEOUT = 180
-MAX_AUTH_FAILURES = 5
 PIR_PIN = 18
 
 gpio.setmode(gpio.BCM)
 gpio.setup(PIR_PIN, gpio.IN)
 
 
-def connect_with_hue(led):
-    try:
-        hue_bridge = lighting.Bridge()
-    except (lighting.BridgeNotFoundException,
-            lighting.IPUtilityException,
-            lighting.BridgeConfigurationException,
-            lighting.BridgeAPIResponseException):
-        led.on('red')
-        return None
-    else:
-        # bridge is authorized...
-        if hue_bridge.authorized:
-            led.blink('green', 3)
-            return hue_bridge
-        else:
-            # bridge needs to authorize..
-            AUTH_FAILURES = 0
-            while not hue_bridge.authorized:
-                # indicate that link button needs to be pressed
-                if hue_bridge.press_link:
-                    led.blink('blue', 3)
-                    # pause to give user time to press link button
-                    time.sleep(5)
-                # attempt authorization...
-                try:
-                    hue_bridge.authorize()
-                except lighting.BridgeAPIResponseException:
-                    led.blink('red', 2)
-                    AUTH_FAILURES += 1
-                    if AUTH_FAILURES >= MAX_AUTH_FAILURES:
-                        led.blink('red', 10)
-                        raise lighting.BridgeAuthAttemptsExceeded
-                    # wait 5 seconds before reattempting after a failure
-                    time.sleep(5)
-                else:
-                    led.blink('green', 3)
-                    return hue_bridge
-
-
 if __name__ == "__main__":
 
     led = LED()
     luminosity_sensor = adafruit.TSL2561()
-    hue_bridge = connect_with_hue(led)
+    hue_bridge = utils.connect_with_hue(led)
 
     while True:
         
--- utils/lighting_helpers.py
+++ utils/lighting_helpers.py
@@ -1,10 +1,53 @@
+import time
 from datetime import datetime
+
+import lighting
 from configuration import ConfigurationManager
 
 
 API_URL = 'http://holly.local/api/system/configuration'
 SYSTEM_NAME = 'Nova5'
 LUMINOSITY_THRESHOLD_DEFAULT = 10
+MAX_AUTH_FAILURES = 5
+
+
+def connect_with_hue(led):
+    try:
+        hue_bridge = lighting.Bridge()
+    except (lighting.BridgeNotFoundException,
+            lighting.IPUtilityException,
+            lighting.BridgeConfigurationException,
+            lighting.BridgeAPIResponseException):
+        led.on('red')
+        return None
+    else:
+        # bridge is authorized...
+        if hue_bridge.authorized:
+            led.blink('green', 3)
+            return hue_bridge
+        else:
+            # bridge needs to authorize..
+            AUTH_FAILURES = 0
+            while not hue_bridge.authorized:
+                # indicate that link button needs to be pressed
+                if hue_bridge.press_link:
+                    led.blink('blue', 3)
+                    # pause to give user time to press link button
+                    time.sleep(5)
+                # attempt authorization...
+                try:
+                    hue_bridge.authorize()
+                except lighting.BridgeAPIResponseException:
+                    led.blink('red', 2)
+                    AUTH_FAILURES += 1
+                    if AUTH_FAILURES >= MAX_AUTH_FAILURES:
+                        led.blink('red', 10)
+                        raise lighting.BridgeAuthAttemptsExceeded
+                    # wait 5 seconds before reattempting after a failure
+                    time.sleep(5)
+                else:
+                    led.blink('green', 3)
+                    return hue_bridge
 
 
 class LightingConfig(object):
