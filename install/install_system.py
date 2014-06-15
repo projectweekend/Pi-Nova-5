8b1da14d33cff9178f1fef845b55c6ce04c97773
Brian Hines <brian@projectweekend.net>
2014-06-15 12:04:06 -0500

    Start work on install script

---
+++ install/install_system.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import subprocess
+
+
+def main():
+    # Install system dependencies
+    subprocess.call(["apt-get", "update"])
+    subprocess.call(["apt-get", "-y", "upgrade"])
+    subprocess.call(["apt-get", "-y", "--force-yes", "install", "upstart"])
+    subprocess.call(["apt-get", "-y", "install", "python-dev"])
+    subprocess.call(["apt-get", "-y", "install", "python-pip"])
+    subprocess.call(["apt-get", "-y", "install", "avahi-daemon"])
+    subprocess.call(["pip", "install", "virtualenv"])
+
+    # Copy Upstart script
+    subprocess.call(["cp", "./install/hue-motion.conf", "/etc/init"])
+
+
+if __name__ == '__main__':
+    main()
